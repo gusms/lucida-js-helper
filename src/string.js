8c7f37d83c25e4a2513147f82ed00b1295eac63a
RafaelPaulo <rafael-sq@hotmail.com>
2016-08-05 16:45:52 -0300

    Revert "Término do lucida.string"
    This reverts commit c72732786e524bcee38fd6d607d79b8e8f894a00.

--- src/string.js
+++ src/string.js
@@ -1,46 +1,30 @@
 'use strict'
 
-lucida.string = {}
+lucida.string = {
 
-lucida.string.sanitize = function(str, only_word_character) {
-    if(typeof str !== 'string') return ''
+    sanitize: function(str, only_word_character) {
 
-    str =str.trim(str)
-            .toLowerCase()
-            .replace(/\s+/g, ' ')
-            .replace(/\s+/g, '_')
-            .replace(/[áàâãåäæª]/g, 'a')
-            .replace(/[éèêëЄ€]/g, 'e')
-            .replace(/[íìîï]/g, 'i')
-            .replace(/[óòôõöøº]/g, 'o')
-            .replace(/[úùûü]/g, 'u')
-            .replace(/[ç¢©]/g, 'c')
+        if(typeof str !== 'string') return ''
 
-    if (only_word_character) {
-        str = str.replace(/^\d+|\d+$/g, '')
-                 .replace(/\d+/g, '_')
-                 .replace(/_+/g, '_')
+        str =str.trim(str)
+                .toLowerCase()
+                .replace(/\s+/g, ' ')
+                .replace(/\s+/g, '_')
+                .replace(/[áàâãåäæª]/g, 'a')
+                .replace(/[éèêëЄ€]/g, 'e')
+                .replace(/[íìîï]/g, 'i')
+                .replace(/[óòôõöøº]/g, 'o')
+                .replace(/[úùûü]/g, 'u')
+                .replace(/[ç¢©]/g, 'c')
+
+        if (only_word_character) {
+            str = str.replace(/^\d+|\d+$/g, '')
+                     .replace(/\d+/g, '_')
+                     .replace(/_+/g, '_')
+        }
+        return str
     }
 
-    return str
-}
 
-lucida.string.trimRight = function(str) {
-    return str.replace(/\s+$/g, '')
-}
 
-lucida.string.trimLeft = function(str) {
-    return str.replace(/^\s+/, '')
-}
-
-lucida.string.trim = function(str) {
-    str = String.prototype.trim ? str.trim() : str.replace(/^\s+|\s+$/g, '');
-	return str;
-}
-
-lucida.string.replaceAll = function(str, token, newtoken) {
-    while (str.indexOf(token) !== -1) {
-		str = str.replace(token, newtoken);
-	}
-	return str;
 }
