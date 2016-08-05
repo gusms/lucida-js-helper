b3156126cf9fd1c6f433924b426a3583b6ecb7d2
RafaelPaulo <rafael-sq@hotmail.com>
2016-08-05 20:01:56 -0300

    Initiating lucida.cookie functionalities

--- tests/spec/spec-cookie.js
+++ tests/spec/spec-cookie.js
@@ -1,7 +1,25 @@
-describe('Módulo lucida.cookie', function() {
+describe('Módulo: lucida.cookie', function() {
 
-    beforeEach(function() {
-        document.cookie = 'yellow=belt; '
+    describe('lucida.cookie.get', function(){
+
+        beforeEach(function() {
+            document.cookie = 'yellow=belt; '
+        })
+
+        afterEach(function() {
+            document.cookie = 'yellow=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+        })
+
+        it('Deve buscar por um cookie pelo seu nome', function() {
+            var cookie = 'yellow'
+            expect(lucida.cookie.get(cookie)).toEqual('belt')
+        })
+
+        it('Deve retornar null quando o cookie procurado não existir', function() {
+            var cookie = 'belt'
+            expect(lucida.cookie.get(cookie)).toEqual(null)
+        })
+
     })
 
 })
