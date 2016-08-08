8aa1b81609e5f3d13b087414c0ca7b4e87367df7
RafaelPaulo <rafael-sq@hotmail.com>
2016-08-08 19:35:22 -0300

    Funcionalidades: lucida.cookie.set/remove/toObject

--- src/cookie.js
+++ src/cookie.js
@@ -3,22 +3,65 @@
 lucida.cookie = {}
 
 lucida.cookie.get = function(key) {
-    var cookie = '; ' + document.cookie,
-		index,
-		end;
+    if(typeof key === 'string'){
+        var cookie = '; ' + document.cookie,
+    		index,
+    		end;
 
-	key = '; ' + key + '=';
-	index = cookie.indexOf(key);
-	if (index !== -1) {
-		cookie = cookie.substring(index + key.length);
-		end = cookie.indexOf(';');
-		return window.unescape(end !== -1 ? cookie.substring(0, end) : cookie);
-	}
+    	key = '; ' + key + '=';
+    	index = cookie.indexOf(key);
+    	if (index !== -1) {
+    		cookie = cookie.substring(index + key.length);
+    		end = cookie.indexOf(';');
+    		return window.unescape(end !== -1 ? cookie.substring(0, end) : cookie);
+    	}
+    }
 	return null;
 }
 
-// lucida.cookie.set
+lucida.cookie.set = function(name, value, opts) {
+    if(name.length > 0){
+    	var cookie,
+            exdate;
 
-// lucida.cookie.remove
+            opts = typeof opts === 'object' ? opts : {
+        		exdays: opts
+        	};
 
-// lucida.cookie.toObject
+        	cookie = '' + name + '=' + (window.escape(value));
+        	if (opts.exdays) {
+        		exdate = new Date();
+        		exdate.setDate(exdate.getDate() + opts.exdays);
+        		cookie += '; expires=' + (exdate.toUTCString());
+        	}
+        	if (opts.domain) {
+        		cookie += '; domain=' + opts.domain;
+        	}
+        	cookie += '; path=' + (opts.path || '/');
+        	document.cookie = cookie;
+    	return cookie;
+    }
+    return null
+}
+
+lucida.cookie.remove = function(name) {
+    var value = lucida.cookie.get(name);
+    document.cookie = name+'=; expires=Thu, 01 Jan 1970 00:00:01 GMT';
+    return value;
+}
+
+lucida.cookie.toObject = function() {
+    if(document.cookie.length > 0){
+        var cookie, cookies, object, t, _i, _len;
+        object = {};
+        cookies = document.cookie.split('; ');
+        for (_i = 0, _len = cookies.length; _i < _len; _i++) {
+            cookie = cookies[_i];
+            t = cookie.split('=');
+            object[t[0]] = decodeURIComponent(t[1]);
+        }
+        return object;
+    }
+
+    return null
+};
--- tests/spec/spec-cookie.js
+++ tests/spec/spec-cookie.js
@@ -4,10 +4,17 @@ describe('Módulo: lucida.cookie', function() {
 
         beforeEach(function() {
             document.cookie = 'yellow=belt; '
+            document.cookie = 'yellow2=belt2; '
         })
 
         afterEach(function() {
             document.cookie = 'yellow=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+            document.cookie = 'yellow2=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+        })
+
+        it('Deve retornar um string se o cookie for encontrado', function() {
+            var cookie = 'yellow'
+            expect(lucida.cookie.get(cookie)).toEqual(jasmine.any(String))
         })
 
         it('Deve buscar por um cookie pelo seu nome', function() {
@@ -17,7 +24,122 @@ describe('Módulo: lucida.cookie', function() {
 
         it('Deve retornar null quando o cookie procurado não existir', function() {
             var cookie = 'belt'
-            expect(lucida.cookie.get(cookie)).toEqual(null)
+            expect(lucida.cookie.get(cookie)).toBeNull()
+        })
+
+        it('Deve retornar null se o parâmetro de entrada não for uma string', function() {
+            var cookie  = 450
+            var cookie2 = {}
+            var cookie3 = []
+            var cookie4
+
+            expect(lucida.cookie.get(cookie)).toBeNull()
+            expect(lucida.cookie.get(cookie2)).toBeNull()
+            expect(lucida.cookie.get(cookie3)).toBeNull()
+            expect(lucida.cookie.get(cookie4)).toBeNull()
+            console.log(lucida.cookie.get(cookie2));
+        })
+
+    })
+
+    describe('lucida.cookie.set', function() {
+
+        beforeEach(function() {
+            document.cookie = 'yellow=belt; '
+        })
+
+        afterEach(function() {
+            document.cookie = 'yellow=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+            document.cookie = 'yellow2=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+        })
+
+        it('Deve criar um cookie', function() {
+            var nomeCookie = 'yellow2'
+            var valorCookie = 'belt2'
+
+            expect(lucida.cookie.set(nomeCookie, valorCookie)).toMatch(/yellow2=belt2/)
+            expect(lucida.cookie.get(nomeCookie)).not.toBeNull()
+        })
+
+        it('Deve deve retornar uma string ao criar um cookie', function() {
+            var nomeCookie = 'yellow2'
+            var valorCookie = 'belt2'
+
+            expect(lucida.cookie.set(nomeCookie, valorCookie)).toEqual(jasmine.any(String))
+        })
+
+        it('Deve deve retornar null caso não seja passando nenhum valor no primeiro parâmetro(nome do cookie)', function() {
+            var nomeCookie = ''
+            var valorCookie = 'belt2'
+
+            expect(lucida.cookie.set(nomeCookie, valorCookie)).toBeNull()
+        })
+
+    })
+
+    describe('lucida.cookie.remove', function() {
+
+        beforeEach(function() {
+            document.cookie = 'yellow=belt; '
+            document.cookie = 'yellow2=belt2; '
+        })
+
+        afterEach(function() {
+            document.cookie = 'yellow=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+            document.cookie = 'yellow2=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+        })
+
+        it('Deve retornar uma string com o valor do cookie excluido', function() {
+            var cookie = 'yellow'
+            expect(lucida.cookie.remove(cookie)).toEqual(jasmine.any(String))
+        })
+
+        it('Deve excluir um cookie pelo nome', function() {
+            var cookie = 'yellow'
+
+            expect(lucida.cookie.remove(cookie)).toEqual('belt')
+            expect(lucida.cookie.get(cookie)).toBeNull()
+        })
+
+        it('Deve retornar null ao tentar excluir um cookie inexistente', function() {
+            var cookie = 'anotherthing'
+            expect(lucida.cookie.remove(cookie)).toBeNull()
+        })
+
+    })
+
+    describe('lucida.cookie.toObject', function() {
+
+        beforeEach(function() {
+            document.cookie = 'yellow=belt; '
+            document.cookie = 'yellow2=belt2; '
+            document.cookie = '=belt2; '
+
+        })
+
+        afterEach(function() {
+            document.cookie = 'yellow=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+            document.cookie = 'yellow2=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+        })
+
+        it('Deve retornar um Objeto com todos os cookies', function(){
+            expect(lucida.cookie.toObject()).toEqual(jasmine.any(Object))
+            expect(lucida.cookie.toObject().yellow).toEqual('belt')
+            expect(lucida.cookie.toObject().yellow2).toEqual('belt2')
+        })
+
+        it('Deve retornar null quando não houver cookies', function(){
+            document.cookie = 'yellow=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+            document.cookie = 'yellow2=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
+            var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+    	var cookie = cookies[i];
+    	var eqPos = cookie.indexOf("=");
+    	var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+    	document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+            expect(lucida.cookie.toObject()).toBeNull()
         })
 
     })
